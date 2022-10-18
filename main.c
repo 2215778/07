273db967fc48ad9fbdecd20a4e8c456127107f9a
2215778 <dlcoals0223@naver.com>
2022-10-18 16:53:06 +0900

    실습4-2

--- main.c
+++ main.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int counter;
+void f(void);
 
-void set_counter(void){
-	counter=20;
+int i; //i가 10까지 증가 
+int main(int argc, char *argv[]) {
+	for(i=0; i<5; i++)
+	{
+		f(); //f 소멸로 5번 반복 못함 
+	}
+	
+	return 0;
 }
 
-int main(int argc, char *argv[]) {
-	printf("counter=%d\n",counter);
-	set_counter();
-	printf("counter=%d\n",counter);
-
-	return 0;
+void f(void){
+	for(i=0; i<10; i++) //main의 i와 이름이  같은 변수 //for문 돌릴때는 지역 변수로 돌리기 
+	    printf("#"); //#을 10번 반복 
 }
