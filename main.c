f71d25d6dd36d68abd371d2070afe7b66d632440
2215778 <dlcoals0223@naver.com>
2022-10-18 16:44:36 +0900

    실습4-1

--- main.c
+++ main.c
@@ -2,22 +2,16 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int counter;
 
-int inc(int counter);
- 
-
-int main(int argc, char *argv[]) {
-	int i=10;
-	printf("함수 호출 전 i=%d\n",i);
-	
-	inc(i); //매개변수가 선언되면 16열
-	printf("함수 호출 후 i=%d\n",i);
-	
-	return 0;
+void set_counter(void){
+	counter=20;
 }
 
-int inc(int counter){  //counter로 매개변수 입력됨,그러나 main 함수에 영향 없음 
-	counter++;
-	
-	return counter;
+int main(int argc, char *argv[]) {
+	printf("counter=%d\n",counter);
+	set_counter();
+	printf("counter=%d\n",counter);
+
+	return 0;
 }
