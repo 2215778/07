160902866418ce8b19fb49de9076b95727a3b037
2215778 <dlcoals0223@naver.com>
2022-10-18 16:25:22 +0900

    실습2

--- main.c
+++ main.c
@@ -4,17 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int flag=1;
-	int y=3; //외부에 y변수를 선언해주기 
+	int i;
 	
-	while(flag!=0)
+	for(i=0; i<5; i++) //5번 반복 
 	{
-		y=3;
-		flag=0;
-	}
-	
-	y=4; //성립하려면 8 열에 y변수 선언 
+		int temp=1;
+		printf("temp= %d\n",temp);
+		temp++; //temp는 중괄호에 소멸, 따라서 temp=1 만 5번 반복
+	 } 
 	return 0;
 }
 
-//y=4;은 불가능, 외부엔 변수 선언밖에 안됨 
