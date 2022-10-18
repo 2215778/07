c0469fdb3fc5940ec4b22cc3ed40e9dc0b6e37a8
2215778 <dlcoals0223@naver.com>
2022-10-18 16:38:57 +0900

    실습3

--- main.c
+++ main.c
@@ -3,15 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int inc(int counter);
+ 
+
 int main(int argc, char *argv[]) {
-	int i;
+	int i=10;
+	printf("함수 호출 전 i=%d\n",i);
 	
-	for(i=0; i<5; i++) //5번 반복 
-	{
-		int temp=1;
-		printf("temp= %d\n",temp);
-		temp++; //temp는 중괄호에 소멸, 따라서 temp=1 만 5번 반복
-	 } 
+	inc(i); //매개변수가 선언되면 16열
+	printf("함수 호출 후 i=%d\n",i);
+	
 	return 0;
 }
 
+int inc(int counter){  //counter로 매개변수 입력됨,그러나 main 함수에 영향 없음 
+	counter++;
+	
+	return counter;
+}
